e954e016f14604f7cdfc0b243fb772ad69beb18d
nix <nix@local>
2024-07-24 21:45:17 +0200

    add convert code

--- vr-alpha.cpp
+++ vr-alpha.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "argparse.hpp"
 #include <vector>
 
 static int detect_rvm(ncnn::Net &net, const cv::Mat &bgr, cv::Mat &pha) {
@@ -33,21 +34,54 @@ const int target_width = 512;
 
 
 int main(int argc, char **argv) {
-    if(argc != 2) {
-        std::cout << "Usage: " << argv[0] << " video_file" << std::endl;
-        return -1;
+    argparse::ArgumentParser program("vr-alpha");
+
+      program.add_argument("video")
+        .help("video path");
+
+    program.add_argument("--eq")
+      .help("input is equirectangular")
+      .default_value(false)
+      .implicit_value(true);
+
+      try {
+        program.parse_args(argc, argv);
+      }
+      catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+      }
+
+    auto inputvideo = program.get<std::string>("video");
+    bool equirectangular = program["--eq"] == true;
+
+    std::string dest = inputvideo;
+    if (dest.size() >= 4) {
+        dest.erase(dest.size() - 4);
     }
-    std::string videopath = argv[1];
-    cv::VideoCapture cap(videopath);
+
+    std::string fisheyevideo = inputvideo;
+    if (equirectangular) {
+        std::string cmd1 = "ffmpeg -i \"";
+        cmd1 += inputvideo;
+        cmd1 += R"(" -filter_complex "[0:v]split=2[left][right]; [left]crop=ih:ih:0:0[left_crop]; [right]crop=ih:ih:ih:0[right_crop]; [left_crop]v360=hequirect:fisheye:iv_fov=180:ih_fov=180:v_fov=180:h_fov=180[leftfisheye]; [right_crop]v360=hequirect:fisheye:iv_fov=180:ih_fov=180:v_fov=180:h_fov=180[rightfisheye]; [leftfisheye][rightfisheye]hstack[v]" -map '[v]' -c:a copy -crf 15 ")";
+        fisheyevideo = dest + "-fisheye.mp4";
+        cmd1 += fisheyevideo;
+        cmd1 += "\"";
+            
+        std::cout << "cmd1: " << cmd1 << std::endl;
+
+        if (int ret = system(cmd1.c_str()); ret != 0)  {
+            return ret;
+        }
+    }
+
+    cv::VideoCapture cap(fisheyevideo);
 
     if(!cap.isOpened()) {
         std::cout << "Cannot open cam" << std::endl;
         return -1;
-    }
-
-    std::string dest = videopath ;
-    if (dest.size() >= 4) {
-        dest.erase(dest.size() - 4);
     }
 
     const int frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT);
@@ -109,17 +143,17 @@ int main(int argc, char **argv) {
     cap.release();
 
 
-    std::string cmd = R"(ffmpeg -i ")";
-    cmd += videopath ;
-    cmd += "\" -i \"";
-    cmd += dest + "-alpha.mp4";
-    cmd += R"(" -i "mask.png" -filter_complex "[1]scale=iw*0.4:-1[alpha];[2][alpha]scale2ref[mask][alpha];[alpha][mask]alphamerge,split=2[masked_alpha1][masked_alpha2]; [masked_alpha1]crop=iw/2:ih:0:0,split=2[masked_alpha_l1][masked_alpha_l2]; [masked_alpha2]crop=iw/2:ih:iw/2:0,split=4[masked_alpha_r1][masked_alpha_r2][masked_alpha_r3][masked_alpha_r4]; [0][masked_alpha_l1]overlay=W*0.5-w*0.5:-0.5*h[out_lt];[out_lt][masked_alpha_l2]overlay=W*0.5-w*0.5:H-0.5*h[out_tb]; [out_tb][masked_alpha_r1]overlay=0-w*0.5:-0.5*h[out_l_lt];[out_l_lt][masked_alpha_r2]overlay=0-w*0.5:H-0.5*h[out_tb_ltb]; [out_tb_ltb][masked_alpha_r3]overlay=W-w*0.5:-0.5*h[out_r_lt];[out_r_lt][masked_alpha_r4]overlay=W-w*0.5:H-0.5*h" -c:v libx265 -crf 17 -preset veryfast ")";
-    cmd += dest + "-result.mp4";
-    cmd += "\" -y";
+    std::string cmd2 = R"(ffmpeg -i ")";
+    cmd2 += fisheyevideo ;
+    cmd2 += "\" -i \"";
+    cmd2 += dest + "-alpha.mp4";
+    cmd2 += R"(" -i "mask.png" -i ")";
+    cmd2 += inputvideo;
+    cmd2 += R"(" -filter_complex "[1]scale=iw*0.4:-1[alpha];[2][alpha]scale2ref[mask][alpha];[alpha][mask]alphamerge,split=2[masked_alpha1][masked_alpha2]; [masked_alpha1]crop=iw/2:ih:0:0,split=2[masked_alpha_l1][masked_alpha_l2]; [masked_alpha2]crop=iw/2:ih:iw/2:0,split=4[masked_alpha_r1][masked_alpha_r2][masked_alpha_r3][masked_alpha_r4]; [0][masked_alpha_l1]overlay=W*0.5-w*0.5:-0.5*h[out_lt];[out_lt][masked_alpha_l2]overlay=W*0.5-w*0.5:H-0.5*h[out_tb]; [out_tb][masked_alpha_r1]overlay=0-w*0.5:-0.5*h[out_l_lt];[out_l_lt][masked_alpha_r2]overlay=0-w*0.5:H-0.5*h[out_tb_ltb]; [out_tb_ltb][masked_alpha_r3]overlay=W-w*0.5:-0.5*h[out_r_lt];[out_r_lt][masked_alpha_r4]overlay=W-w*0.5:H-0.5*h" -c:v libx265 -crf 17 -preset veryfast -map 3:a:? -c:a copy ")";
+    cmd2 += dest + "-result.mp4";
+    cmd2 += "\" -y";
 
-    std::cout << "cmd: " << cmd << std::endl;
+    std::cout << "cmd2: " << cmd2 << std::endl;
 
-    system(cmd.c_str());
-
-    return 0;
+    return system(cmd2.c_str());
 }
