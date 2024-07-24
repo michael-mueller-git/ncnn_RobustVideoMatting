41d8d34d38af33850c1ce9065a1d39fa57fe888b
nix <nix@local>
2024-07-24 20:47:11 +0200

    add vr alpha extractor

--- vr-alpha.cpp
+++ vr-alpha.cpp
@@ -5,26 +5,33 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 
-static void draw_objects(const cv::Mat &bgr, const cv::Mat &fgr, const cv::Mat &pha, cv::Mat &comp) {
+/*
+static int detect_rvm(ncnn::Net &net, const cv::Mat &bgr, cv::Mat &pha) {
+const int target_width = 512;
+    const int target_height = target_width;
 
-    // cv::Mat comp;
-    cv::resize(bgr, comp, pha.size(), 0, 0, 1);
-    for(int i = 0; i < pha.rows; i++) {
-        for(int j = 0; j < pha.cols; j++) {
-            uchar data = pha.at<uchar>(i, j);
-            float alpha = (float)data / 255;
-            comp.at<cv::Vec3b>(i, j)[0] = fgr.at<cv::Vec3b>(i, j)[0] * alpha + (1 - alpha) * 255;
-            comp.at<cv::Vec3b>(i, j)[1] = fgr.at<cv::Vec3b>(i, j)[1] * alpha + (1 - alpha) * 155;
-            comp.at<cv::Vec3b>(i, j)[2] = fgr.at<cv::Vec3b>(i, j)[2] * alpha + (1 - alpha) * 120;
-        }
-    }
-#if 0
-    cv::imshow("pha", pha);   // alphaチャンネル
-    cv::imshow("fgr", fgr);   // 合成前の画像
-    cv::imshow("comp", comp); // 合成画像
-    const int key = cv::waitKey(1);
-#endif
+    ncnn::Extractor ex = net.create_extractor();
+    const float mean_vals[3] = {0, 0, 0};
+    const float norm_vals[3] = {1 / 255.0, 1 / 255.0, 1 / 255.0};
+    ncnn::Mat ncnn_in1 = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, bgr.cols, bgr.rows, target_width, target_height);
+
+    ncnn_in1.substract_mean_normalize(mean_vals, norm_vals);
+    ex.input("in0", ncnn_in1);
+    ncnn::Mat pha_;
+    ex.extract("out1", pha_);
+
+    cv::Mat cv_pha = cv::Mat(pha_.h, pha_.w, CV_32FC1, (float *)pha_.data);
+
+    resize(cv_pha, cv_pha, cv::Size(bgr.cols, bgr.rows), cv::INTER_LINEAR);
+
+    cv::Mat pha8U;
+    cv_pha.convertTo(pha8U, CV_8UC1, 255.0, 0);
+
+    pha8U.copyTo(pha);
+
+    return 0;
 }
+*/
 
 static int detect_rvm(ncnn::Net &net, const cv::Mat &bgr, cv::Mat &pha, cv::Mat &fgr) {
     const int target_width = 512;
@@ -78,14 +85,17 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    //cap.set(cv::CAP_PROP_FOURCC, cv::CAP_OPENCV_MJPEG);
-    //cap.set(cv::CAP_PROP_FRAME_WIDTH, 480*2);
-    //cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    const int frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT);
 
-    ncnn::Net net;
-    net.opt.use_vulkan_compute = true;
-    net.load_param("rvm_ts.ncnn.param");
-    net.load_model("rvm_ts.ncnn.bin");
+    ncnn::Net left_net;
+    left_net.opt.use_vulkan_compute = true;
+    left_net.load_param("rvm_ts.ncnn.param");
+    left_net.load_model("rvm_ts.ncnn.bin");
+
+    ncnn::Net right_net;
+    right_net.opt.use_vulkan_compute = true;
+    right_net.load_param("rvm_ts.ncnn.param");
+    right_net.load_model("rvm_ts.ncnn.bin");
 
     double scale = 0.25;
 
@@ -93,12 +103,11 @@ int main(int argc, char **argv) {
 
     std::cout << frame_size << std::endl;
 
-    cv::VideoWriter out("output.mp4", cv::CAP_FFMPEG, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), cap.get(cv::CAP_PROP_FPS), frame_size, false);
+    cv::VideoWriter out(videopath + "-alpha.mp4", cv::CAP_FFMPEG, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), cap.get(cv::CAP_PROP_FPS), frame_size, false);
 
     int i = 0;
     while(cap.isOpened()) {
-        i++;
-        std::cout << i << std::endl;
+        std::cout << i++ << "/" << frame_count << std::endl;
         cv::Mat frame, resized_frame;
 
         cap >> frame;
@@ -108,27 +117,27 @@ int main(int argc, char **argv) {
 
         cv::resize(frame, resized_frame, cv::Size(), scale, scale, cv::INTER_AREA);
 
-        std::cout << resized_frame.size() << std::endl;
+        cv::Mat left_frame;
+        resized_frame(cv::Rect(0, 0, resized_frame.cols/2, resized_frame.rows)).copyTo(left_frame);
 
-        cv::Mat fgr, pha, comp;
-        detect_rvm(net, resized_frame, fgr, pha);
-        //draw_objects(frame, fgr, pha, comp); // 后处理
+        cv::Mat right_frame;
+        resized_frame(cv::Rect(resized_frame.cols/2, 0, resized_frame.cols/2, resized_frame.rows)).copyTo(right_frame);
 
-     cv::resize(resized_frame, comp, pha.size(), 0, 0, 1);
-    for(int i = 0; i < pha.rows; i++) {
-        for(int j = 0; j < pha.cols; j++) {
-            uchar data = pha.at<uchar>(i, j);
-            float alpha = (float)data / 255;
-            comp.at<cv::Vec3b>(i, j)[0] = fgr.at<cv::Vec3b>(i, j)[0] * alpha + (1 - alpha) * 255;
-            comp.at<cv::Vec3b>(i, j)[1] = fgr.at<cv::Vec3b>(i, j)[1] * alpha + (1 - alpha) * 155;
-            comp.at<cv::Vec3b>(i, j)[2] = fgr.at<cv::Vec3b>(i, j)[2] * alpha + (1 - alpha) * 120;
-        }
-    }
+        // std::cout << resized_frame.size() << std::endl;
 
-        out.write(fgr);
-        cv::imshow("x", fgr);
-        const int key = cv::waitKey(1);
+        cv::Mat left_pha, right_pha;
+        cv::Mat left_fgr, right_fgr;
 
+        detect_rvm(left_net, left_frame, left_fgr, left_pha);
+        detect_rvm(left_net, right_frame, right_fgr, right_pha);
+
+        std::vector<cv::Mat> alpha_stack = {left_fgr, right_fgr};
+
+        cv::Mat alpha;
+
+        cv::hconcat(alpha_stack, alpha);
+
+        out.write(alpha);
     }
     out.release();
     cap.release();
