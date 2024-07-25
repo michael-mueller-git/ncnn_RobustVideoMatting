389f5acc14c04bda3d28755c793e0bc18a9253ed
nix <nix@local>
2024-07-25 17:33:25 +0200

    add suport for large frames

--- vr-alpha.cpp
+++ vr-alpha.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "argparse.hpp"
 #include <vector>
+#include <fstream>
 
 static int detect_rvm(ncnn::Net &net, const cv::Mat &bgr, cv::Mat &pha) {
 const int target_width = 512;
@@ -32,6 +33,10 @@ const int target_width = 512;
     return 0;
 }
 
+bool fileExists(const std::string& filename) {
+    std::ifstream file(filename);
+    return file.good();
+}
 
 int main(int argc, char **argv) {
     argparse::ArgumentParser program("vr-alpha");
@@ -39,12 +44,14 @@ int main(int argc, char **argv) {
       program.add_argument("video")
         .help("video path");
 
-    program.add_argument("--eq")
-      .help("input is equirectangular")
-      .default_value(false)
-      .implicit_value(true);
+    program.add_argument("type")
+      .choices("eq", "fisheye180", "fisheye190", "fisheye200");
 
-      try {
+    program.add_argument("--scale")
+        .default_value(0.25)
+        .scan<'g', double>();
+    
+    try {
         program.parse_args(argc, argv);
       }
       catch (const std::exception& err) {
@@ -54,15 +61,21 @@ int main(int argc, char **argv) {
       }
 
     auto inputvideo = program.get<std::string>("video");
-    bool equirectangular = program["--eq"] == true;
+    auto videotype = program.get<std::string>("type");
+    double scale =  program.get<double>("--scale");
 
+    if (!fileExists(inputvideo)) {
+        std::cout << "Input video not found" << std::endl;
+        return -1;
+    }
+        
     std::string dest = inputvideo;
     if (dest.size() >= 4) {
         dest.erase(dest.size() - 4);
     }
 
     std::string fisheyevideo = inputvideo;
-    if (equirectangular) {
+    if (videotype == "eq") { 
         std::string cmd1 = "ffmpeg -i \"";
         cmd1 += inputvideo;
         cmd1 += R"(" -filter_complex "[0:v]split=2[left][right]; [left]crop=ih:ih:0:0[left_crop]; [right]crop=ih:ih:ih:0[right_crop]; [left_crop]v360=hequirect:fisheye:iv_fov=180:ih_fov=180:v_fov=180:h_fov=180[leftfisheye]; [right_crop]v360=hequirect:fisheye:iv_fov=180:ih_fov=180:v_fov=180:h_fov=180[rightfisheye]; [leftfisheye][rightfisheye]hstack[v]" -map '[v]' -c:a copy -crf 15 ")";
@@ -75,6 +88,11 @@ int main(int argc, char **argv) {
         if (int ret = system(cmd1.c_str()); ret != 0)  {
             return ret;
         }
+    } else if (videotype == "fisheye180") {
+        // Nothing to do here
+    } else{
+        std::cout << "Not implemented" << std::endl;
+        return  -1;
     }
 
     cv::VideoCapture cap(fisheyevideo);
@@ -96,13 +114,12 @@ int main(int argc, char **argv) {
     right_net.load_param("rvm_ts.ncnn.param");
     right_net.load_model("rvm_ts.ncnn.bin");
 
-    double scale = 0.25;
 
     cv::Size frame_size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)); 
 
     std::cout << frame_size << std::endl;
 
-    cv::VideoWriter out(dest + "-alpha.mp4", cv::CAP_FFMPEG, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), cap.get(cv::CAP_PROP_FPS), frame_size, false);
+    cv::VideoWriter out(dest + "-alpha.avi", cv::CAP_FFMPEG, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), cap.get(cv::CAP_PROP_FPS), frame_size, false);
 
     int i = 0;
     while(cap.isOpened()) {
@@ -146,7 +163,7 @@ int main(int argc, char **argv) {
     std::string cmd2 = R"(ffmpeg -i ")";
     cmd2 += fisheyevideo ;
     cmd2 += "\" -i \"";
-    cmd2 += dest + "-alpha.mp4";
+    cmd2 += dest + "-alpha.avi";
     cmd2 += R"(" -i "mask.png" -i ")";
     cmd2 += inputvideo;
     cmd2 += R"(" -filter_complex "[1]scale=iw*0.4:-1[alpha];[2][alpha]scale2ref[mask][alpha];[alpha][mask]alphamerge,split=2[masked_alpha1][masked_alpha2]; [masked_alpha1]crop=iw/2:ih:0:0,split=2[masked_alpha_l1][masked_alpha_l2]; [masked_alpha2]crop=iw/2:ih:iw/2:0,split=4[masked_alpha_r1][masked_alpha_r2][masked_alpha_r3][masked_alpha_r4]; [0][masked_alpha_l1]overlay=W*0.5-w*0.5:-0.5*h[out_lt];[out_lt][masked_alpha_l2]overlay=W*0.5-w*0.5:H-0.5*h[out_tb]; [out_tb][masked_alpha_r1]overlay=0-w*0.5:-0.5*h[out_l_lt];[out_l_lt][masked_alpha_r2]overlay=0-w*0.5:H-0.5*h[out_tb_ltb]; [out_tb_ltb][masked_alpha_r3]overlay=W-w*0.5:-0.5*h[out_r_lt];[out_r_lt][masked_alpha_r4]overlay=W-w*0.5:H-0.5*h" -c:v libx265 -crf 17 -preset veryfast -map 3:a:? -c:a copy ")";
